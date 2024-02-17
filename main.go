b4300adc8c27f17bf31a0482b1481914f750a6bc
John Freeman <jlfreeman@fastmail.com>
2024-02-17 12:15:48 -0500

    Setup external packages

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-	""
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/Noisey96/budget-tracker/templates"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	component := hello("John")
-	component.Render(context.Background(), os.Stdout)
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", HomeHandler)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
+
+// Handler
+func HomeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, templates.Hello("John"))
 }
